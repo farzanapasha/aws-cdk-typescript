7d9ca4415a413e25d2476f8907d947effebf68ad
Farzana Pasha Jahangeer <farzanapasha.farzana@gmail.com>
2025-03-22 14:32:03 -0400

    updated for testing

--- lib/fjahangeer-cdk-project-stack.ts
+++ lib/fjahangeer-cdk-project-stack.ts
@@ -11,7 +11,7 @@ export class FjahangeerCdkProjectStack extends cdk.Stack {
 
 
    // 🔹 Create an Amazon S3 Bucket
-   const FjahBucket = new s3.Bucket(this, 'CdkTestS3Bucket', {
+   const FjahBucket = new s3.Bucket(this, 'CdkLabS3Bucket', {
      versioned: true,
      removalPolicy: cdk.RemovalPolicy.DESTROY,
    });
